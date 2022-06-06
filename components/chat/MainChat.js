76558b65cde7e3aaa0baf30d35a1f3b0cb1fca72
Sorot Charoensinchai <86677452+taemoolopliam@users.noreply.github.com>
2022-06-06 16:40:33 +0700

    Config : tailwindCSS postcss

---
+++ components/chat/MainChat.js
@@ -0,0 +1,3 @@
+export default function MainChat() {
+  return <div className=" bg-slate-700 w-3 h-3">MainChat</div>;
+}
--- components/layouts/public/PublicLayout.js
+++ components/layouts/public/PublicLayout.js
@@ -1,11 +1,13 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import MainChat from "../../chat/MainChat";
 
 export default function publicLayout({ children }) {
   return (
     <>
       <Navbar />
       {children}
+      <MainChat />
       <Footer />
     </>
   );
