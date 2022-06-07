3ef2968926a3fec71672f9ace1063c89fb9d35fb
Sorot Charoensinchai <86677452+taemoolopliam@users.noreply.github.com>
2022-06-07 13:52:10 +0700

    Feat : Add UI

--- components/chat/MainChat.js
+++ components/chat/MainChat.js
@@ -1,7 +1,16 @@
 export default function MainChat() {
   return (
-    <button className="bottom-0 right-0 fixed z-20 m-5   bg-green-700 text-white  rounded-md w-20 text-center p-1">
-      ช่วยเหลือ
-    </button>
+    // <button className="bottom-0 right-0 fixed z-20 m-5   bg-green-700 text-white  rounded-md w-20 text-center p-1">
+    //   ช่วยเหลือ
+    // </button>
+    <div className="bottom-0 right-0 fixed   z-20 m-5 ">
+      <div className=" bg-white w-96 drop-shadow-md">
+        <div className=" bg-green-700 text-center p-2 text-white font-medium rounded-t-lg">Demo Live Chat</div>
+        <div className="p-5">
+          <label htmlFor="email">อีเมล</label>
+          <input type="text" id="email" />
+        </div>
+      </div>
+    </div>
   );
 }
