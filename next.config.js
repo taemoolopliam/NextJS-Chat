07cbf66d5f2ca94edcb7907cdfbb7063eb359339
Sorot Charoensinchai <86677452+taemoolopliam@users.noreply.github.com>
2022-06-07 13:53:39 +0700

    Feat : add font  and optimize font

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  optimizeFonts: true,
+  distDir: 'build',
+  useFileSystemPublicRoutes: false,
 }
 
 module.exports = nextConfig
