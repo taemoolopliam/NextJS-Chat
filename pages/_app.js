22066def7ca1383718f7b7714351bb035b492485
Sorot Charoensinchai <86677452+taemoolopliam@users.noreply.github.com>
2022-06-07 15:50:49 +0700

    Config : tailwind enable jit mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
-import '../styles/tailwind.css'
-
-// import "../styles/globals.css";
+import "../styles/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
