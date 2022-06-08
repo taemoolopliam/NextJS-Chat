e4e99a15411685d0ea492f4595400bc3d9609eb8
Sorot Charoensinchai <86677452+taemoolopliam@users.noreply.github.com>
2022-06-08 17:25:22 +0700

    Test : Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
@@ -22,29 +23,20 @@ class MyDocument extends Document {
               __html: "body{font-family: 'Sarabun', sans-serif;}",
             }}
           />
-          {/* <!-- Google Tag Manager --> */}
-          <script>
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KMQZPSM'); `}
-          </script>
-          {/* <!-- End Google Tag Manager --> */}
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-4Q6EEEJ132" strategy="afterInteractive" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+          </Script>
         </Head>
         <body>
           <Main />
           <NextScript />
-          {/* <!-- Google Tag Manager (noscript) --> */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-KMQZPSM"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
-            ></iframe>
-          </noscript>
-          {/* <!-- End Google Tag Manager (noscript) --> */}
         </body>
       </Html>
     );
