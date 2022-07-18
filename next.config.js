bfec296ed791a30177a17d0336f79becc0ea0f9b
Sorot Charoensinchai <86677452+taemoolopliam@users.noreply.github.com>
2022-07-18 17:34:26 +0700

    Test : Chat

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 module.exports = withBundleAnalyzer({});
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   optimizeFonts: true,
   // distDir: 'build',
   // useFileSystemPublicRoutes: false,
