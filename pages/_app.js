8d27471a78952876b9a54fc39117244db2dd0440
Sorot Charoensinchai <86677452+taemoolopliam@users.noreply.github.com>
2022-07-18 10:31:25 +0700

    Fix : Single Shared Layout with Custom App

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 // import '../styles/tailwind.css'
 
 import "../styles/globals.css";
+import PublicLayout from "../components/layouts/public/PublicLayout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <PublicLayout>
+      <Component {...pageProps} />
+    </PublicLayout>
+  );
 }
 
 export default MyApp;
